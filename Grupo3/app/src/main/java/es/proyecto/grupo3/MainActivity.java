86a4751ea0541bb1387412b14edbca39e45d768e
Ikerisasi <iker.isasian@elorrieta-errekamari.com>
2022-02-25 14:09:30 +0100

    añadido main

--- Grupo3/app/src/main/java/es/proyecto/grupo3/MainActivity.java
+++ Grupo3/app/src/main/java/es/proyecto/grupo3/MainActivity.java
@@ -2,7 +2,9 @@ package es.proyecto.grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnProductos = findViewById(R.id.btnProductos);
+        Button btnTiendas = findViewById(R.id.btnTiendas);
+        Button btnTenderos = findViewById(R.id.btnTenderos);
+
+        btnProductos.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, ProductosActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTiendas.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, TiendasActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTenderos.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, PerfilActivity.class);
+            startActivity(intent);
+        } );
+
     }
 }
