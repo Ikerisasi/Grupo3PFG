8dba03e8bd4b5c642663947166fc92b4b8288314
Ikerisasi <iker.isasian@elorrieta-errekamari.com>
2022-02-25 13:02:12 +0100

    añadida PerfilActivity

--- Grupo3/app/src/main/java/es/proyecto/grupo3/ProductosActivity.java
+++ Grupo3/app/src/main/java/es/proyecto/grupo3/ProductosActivity.java
@@ -2,7 +2,9 @@ package es.proyecto.grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProductosActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class ProductosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+
+        Button btnProductosProducto = findViewById(R.id.btnProductosProducto);
+        Button btnTiendasProducto = findViewById(R.id.btnTiendasProducto);
+        Button btnTenderosProducto = findViewById(R.id.btnTenderosProducto);
+
+        btnProductosProducto.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, ProductosActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTiendasProducto.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, TiendasActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTenderosProducto.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, PerfilActivity.class);
+            startActivity(intent);
+        } );
+
     }
 }
--- Grupo3/app/src/main/java/es/proyecto/grupo3/TiendasActivity.java
+++ Grupo3/app/src/main/java/es/proyecto/grupo3/TiendasActivity.java
@@ -2,7 +2,9 @@ package es.proyecto.grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TiendasActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class TiendasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiendas);
+
+        Button btnProductosTienda = findViewById(R.id.btnProductosTienda);
+        Button btnTiendasTienda = findViewById(R.id.btnTiendasTienda);
+        Button btnTenderosTienda = findViewById(R.id.btnTenderosTienda);
+
+        btnProductosTienda.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, ProductosActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTiendasTienda.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, TiendasActivity.class);
+            startActivity(intent);
+        } );
+
+        btnTenderosTienda.setOnClickListener( (v) -> {
+            Intent intent = new Intent(this, PerfilActivity.class);
+            startActivity(intent);
+        } );
+
     }
 }
