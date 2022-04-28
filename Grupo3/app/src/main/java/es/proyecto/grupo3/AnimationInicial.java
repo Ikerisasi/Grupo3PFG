647907f79409467b491efcacc2e98eb701e6cb68
unknown <pablo.gonzalezpr@elorrieta-errekamari.com>
2022-04-28 17:14:03 +0200

    cambios varios

---
+++ Grupo3/app/src/main/java/es/proyecto/grupo3/AnimationInicial.java
@@ -0,0 +1,48 @@
+package es.proyecto.grupo3;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AnimationInicial extends AppCompatActivity {
+
+    public LottieAnimationView lottieAnimationView = null ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_inicial);
+
+        lottieAnimationView  = findViewById(R.id.imageView);
+        animacion(lottieAnimationView, R.raw.loading);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(AnimationInicial.this, IdiomasActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,3600);
+    }
+
+    public void animacion( LottieAnimationView imageView, Integer animation ){
+
+
+        imageView.setAnimation(animation);
+        imageView.playAnimation();
+        imageView.setRepeatCount(1);
+
+    }
+}
