04323e8e0408b08250412abf32d0178bfc53e30b
unknown <pablo.gonzalezpr@elorrieta-errekamari.com>
2022-05-16 18:35:40 +0200

    Cambios de diseño

--- Grupo3/app/src/main/java/es/proyecto/grupo3/db/DBManager.java
+++ Grupo3/app/src/main/java/es/proyecto/grupo3/db/DBManager.java
@@ -104,8 +104,8 @@ public class DBManager extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(CREATE_TABLE_TENDERO);
 
         ContentValues values1 = new ContentValues();
-        values1.put(NOMBRE_TENDERO, "joseluis");
-        values1.put(PASSWORD_TENDERO, "joseluis");
+        values1.put(NOMBRE_TENDERO, "Chun Li");
+        values1.put(PASSWORD_TENDERO, "ChunLi");
 
         ContentValues values2 = new ContentValues();
         values2.put(NOMBRE_TENDERO, "paco");
@@ -124,8 +124,8 @@ public class DBManager extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(CREATE_TABLE_TIENDA);
 
         ContentValues values10 = new ContentValues();
-        values10.put(NOMBRE_TIENDA, "mediamarkt");
-        values10.put(DESCRIPCION_TIENDA, "tienda de informatica");
+        values10.put(NOMBRE_TIENDA, "Bazar");
+        values10.put(DESCRIPCION_TIENDA, "tienda de todo");
         values10.put(LOCALIZACION, "MegaPark");
         values10.put(CALLE, "Av. de la Ribera 5, Barakaldo");
         values10.put(LONGITUD, 43.290313868882826);
@@ -133,26 +133,26 @@ public class DBManager extends SQLiteOpenHelper {
         values10.put(ID_TIENDAS_TENDERO, 1);
 
         ContentValues values11 = new ContentValues();
-        values11.put(NOMBRE_TIENDA, "zara");
-        values11.put(DESCRIPCION_TIENDA, "tienda de ropa");
-        values11.put(LOCALIZACION, "Abando");
-        values11.put(CALLE, "Don Diego López Haroko 23, Bilbao");
+        values11.put(NOMBRE_TIENDA, "Game");
+        values11.put(DESCRIPCION_TIENDA, "Tienda de videojuegos");
+        values11.put(LOCALIZACION, "Zubiarte");
+        values11.put(CALLE, "Leizaola Lehendakariaren Kalea 2, Bilbao");
         values11.put(LONGITUD, 43.26209185329112);
         values11.put(LATITUD, -2.9314099935629185);
         values11.put(ID_TIENDAS_TENDERO, 2);
 
         ContentValues values12 = new ContentValues();
-        values12.put(NOMBRE_TIENDA, "Game");
-        values12.put(DESCRIPCION_TIENDA, "Tienda de videojuegos");
-        values12.put(LOCALIZACION, "Zubiarte");
-        values12.put(CALLE, "Leizaola Lehendakariaren Kalea 2, Bilbao");
+        values12.put(NOMBRE_TIENDA, "Mercules");
+        values12.put(DESCRIPCION_TIENDA, "Tienda de moda");
+        values12.put(LOCALIZACION, "Abando");
+        values12.put(CALLE, "Don Diego López Haroko 23, Bilbao");
         values12.put(LONGITUD, 43.267771849803424);
         values12.put(LATITUD, -2.94022183764494);
-        values12.put(ID_TIENDAS_TENDERO, 3);
+        values12.put(ID_TIENDAS_TENDERO, 2);
 
         ContentValues values13 = new ContentValues();
-        values13.put(NOMBRE_TIENDA, "Lacoste");
-        values13.put(DESCRIPCION_TIENDA, "Tienda de videojuegos");
+        values13.put(NOMBRE_TIENDA, "Deportes - Hnos. Gutiérrez");
+        values13.put(DESCRIPCION_TIENDA, "Tienda de Ropa");
         values13.put(LOCALIZACION, "Zubiarte");
         values13.put(CALLE, "Leizaola Lehendakariaren Kalea 2, Bilbao");
         values13.put(LONGITUD, 41.267771849803424);
@@ -160,19 +160,29 @@ public class DBManager extends SQLiteOpenHelper {
         values13.put(ID_TIENDAS_TENDERO, 3);
 
         ContentValues values14 = new ContentValues();
-        values14.put(NOMBRE_TIENDA, "Churrimuski");
-        values14.put(DESCRIPCION_TIENDA, "Tienda de videojuegos");
+        values14.put(NOMBRE_TIENDA, "Zapateria Loli");
+        values14.put(DESCRIPCION_TIENDA, "Tienda de Ropa");
         values14.put(LOCALIZACION, "Zubiarte");
         values14.put(CALLE, "Leizaola Lehendakariaren Kalea 2, Bilbao");
         values14.put(LONGITUD, 42.267771849803424);
         values14.put(LATITUD, -4.94022183764494);
         values14.put(ID_TIENDAS_TENDERO, 3);
 
+        ContentValues values15 = new ContentValues();
+        values15.put(NOMBRE_TIENDA,"Peluquerias Paqui");
+        values15.put(DESCRIPCION_TIENDA,"Peluqueria");
+        values15.put(LOCALIZACION,"Zubiarte");
+        values15.put(CALLE,"Don Diego López Haroko 23, Bilbao");
+        values15.put(LONGITUD,42.2677718496703424);
+        values15.put(LATITUD,3.267771849803424);
+        values15.put(ID_TIENDAS_TENDERO,3);
+
         sqLiteDatabase.insert(TABLE_TIENDA, null, values10);
         sqLiteDatabase.insert(TABLE_TIENDA, null, values11);
         sqLiteDatabase.insert(TABLE_TIENDA, null, values12);
         sqLiteDatabase.insert(TABLE_TIENDA, null, values13);
         sqLiteDatabase.insert(TABLE_TIENDA, null, values14);
+        sqLiteDatabase.insert(TABLE_TIENDA,null, values15);
 
         //Creación tabla categorías + información predeterminada
 
@@ -188,56 +198,187 @@ public class DBManager extends SQLiteOpenHelper {
         values23.put(NOMBRE_CATEGORIA, "Deporte");
         ContentValues values24 = new ContentValues();
         values24.put(NOMBRE_CATEGORIA, "Mobiliario");
+        ContentValues values25 = new ContentValues();
+        values25.put(NOMBRE_CATEGORIA, "Variados");
 
         sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values20);
         sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values21);
         sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values22);
         sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values23);
         sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values24);
+        sqLiteDatabase.insert(TABLE_CATEGORIAS, null, values25);
 
         //Creación tabla productos + información predeterminada
         sqLiteDatabase.execSQL(CREATE_TABLE_PRODUCTOS);
 
         ContentValues values31 = new ContentValues();
-        values31.put(NOMBRE_PRODUCTO, "Poducto");
-        values31.put(DESCRIPCION_PRODUCTO, "muy grande");
-        values31.put(PRECIO_PRODUCTO, 14.92);
-        values31.put(ID_PRODUCTOS_TIENDA, 3);
-        values31.put(ID_PRODUCTOS_CATEGORIA, 1);
+        values31.put(NOMBRE_PRODUCTO, "Sangre Falsa");
+        values31.put(DESCRIPCION_PRODUCTO, "Tarro de 450ml");
+        values31.put(PRECIO_PRODUCTO, 4.99);
+        values31.put(ID_PRODUCTOS_TIENDA, 1);
+        values31.put(ID_PRODUCTOS_CATEGORIA, 6);
 
         ContentValues values32 = new ContentValues();
-        values32.put(NOMBRE_PRODUCTO, "Bicicleta");
-        values32.put(DESCRIPCION_PRODUCTO, "muy pequeñito");
-        values32.put(PRECIO_PRODUCTO, 92.35);
-        values32.put(ID_PRODUCTOS_TIENDA, 4);
-        values32.put(ID_PRODUCTOS_CATEGORIA, 2);
+        values32.put(NOMBRE_PRODUCTO, "Pajitas");
+        values32.put(DESCRIPCION_PRODUCTO, "24 unidades/pedido");
+        values32.put(PRECIO_PRODUCTO, 1.95);
+        values32.put(ID_PRODUCTOS_TIENDA, 1);
+        values32.put(ID_PRODUCTOS_CATEGORIA, 6);
 
         ContentValues values33 = new ContentValues();
-        values33.put(NOMBRE_PRODUCTO, "Lavadora");
-        values33.put(DESCRIPCION_PRODUCTO, "videojuego");
-        values33.put(PRECIO_PRODUCTO, 69.99);
-        values33.put(ID_PRODUCTOS_TIENDA, 3);
-        values33.put(ID_PRODUCTOS_CATEGORIA, 3);
+        values33.put(NOMBRE_PRODUCTO, "Collar de perro");
+        values33.put(DESCRIPCION_PRODUCTO, "Dimensiones: 1.5x38cm");
+        values33.put(PRECIO_PRODUCTO, 1.45);
+        values33.put(ID_PRODUCTOS_TIENDA, 1);
+        values33.put(ID_PRODUCTOS_CATEGORIA, 6);
 
         ContentValues values34 = new ContentValues();
-        values34.put(NOMBRE_PRODUCTO, "POLLO");
-        values34.put(DESCRIPCION_PRODUCTO, "muy pequeñito");
-        values34.put(PRECIO_PRODUCTO, 92.35);
-        values34.put(ID_PRODUCTOS_TIENDA, 4);
-        values34.put(ID_PRODUCTOS_CATEGORIA, 3);
+        values34.put(NOMBRE_PRODUCTO, "Cojin de viaje");
+        values34.put(DESCRIPCION_PRODUCTO, "Cojín spandex");
+        values34.put(PRECIO_PRODUCTO,  6.00);
+        values34.put(ID_PRODUCTOS_TIENDA, 1);
+        values34.put(ID_PRODUCTOS_CATEGORIA, 6);
 
         ContentValues values35 = new ContentValues();
-        values35.put(NOMBRE_PRODUCTO, "patata");
-        values35.put(DESCRIPCION_PRODUCTO, "muy pequeñito");
-        values35.put(PRECIO_PRODUCTO, 92.35);
-        values35.put(ID_PRODUCTOS_TIENDA, 3);
-        values35.put(ID_PRODUCTOS_CATEGORIA, 1);
+        values35.put(NOMBRE_PRODUCTO, "Velas aromatizantes");
+        values35.put(DESCRIPCION_PRODUCTO, "Aroma a Fresa");
+        values35.put(PRECIO_PRODUCTO, 2.75);
+        values35.put(ID_PRODUCTOS_TIENDA, 1);
+        values35.put(ID_PRODUCTOS_CATEGORIA, 6);
+
+        ContentValues values36 = new ContentValues();
+        values36.put(NOMBRE_PRODUCTO, "Transportin");
+        values36.put(DESCRIPCION_PRODUCTO, "Transporte para animales");
+        values36.put(PRECIO_PRODUCTO, 19.20);
+        values36.put(ID_PRODUCTOS_TIENDA, 1);
+        values36.put(ID_PRODUCTOS_CATEGORIA, 6);
+
+        ContentValues values37 = new ContentValues();
+        values37.put(NOMBRE_PRODUCTO, "Raton");
+        values37.put(DESCRIPCION_PRODUCTO, "Ratón óptico Deep");
+        values37.put(PRECIO_PRODUCTO, 3.25);
+        values37.put(ID_PRODUCTOS_TIENDA, 1);
+        values37.put(ID_PRODUCTOS_CATEGORIA, 6);
+
+        ContentValues values38 = new ContentValues();
+        values38.put(NOMBRE_PRODUCTO, "Play Station 4");
+        values38.put(DESCRIPCION_PRODUCTO, "Consola");
+        values38.put(PRECIO_PRODUCTO, 269);
+        values38.put(ID_PRODUCTOS_TIENDA, 2);
+        values38.put(ID_PRODUCTOS_CATEGORIA, 1);
+
+        ContentValues values39 = new ContentValues();
+        values39.put(NOMBRE_PRODUCTO, "Mortal Kombat 11");
+        values39.put(DESCRIPCION_PRODUCTO, "Videojuego de lucha");
+        values39.put(PRECIO_PRODUCTO, 36);
+        values39.put(ID_PRODUCTOS_TIENDA, 2);
+        values39.put(ID_PRODUCTOS_CATEGORIA, 1);
+
+        ContentValues values40 = new ContentValues();
+        values40.put(NOMBRE_PRODUCTO, "Controller Xbox");
+        values40.put(DESCRIPCION_PRODUCTO, "Mando inalambrico");
+        values40.put(PRECIO_PRODUCTO, 50);
+        values40.put(ID_PRODUCTOS_TIENDA, 2);
+        values40.put(ID_PRODUCTOS_CATEGORIA, 1);
+
+        ContentValues values41 = new ContentValues();
+        values41.put(NOMBRE_PRODUCTO, "Sandalias Atenea");
+        values41.put(DESCRIPCION_PRODUCTO, "Sandalias de madera");
+        values41.put(PRECIO_PRODUCTO, 125);
+        values41.put(ID_PRODUCTOS_TIENDA, 3);
+        values41.put(ID_PRODUCTOS_CATEGORIA, 2);
+
+        ContentValues values42 = new ContentValues();
+        values42.put(NOMBRE_PRODUCTO, "Cinturón Rojo");
+        values42.put(DESCRIPCION_PRODUCTO, "Cinturón Fender rojo");
+        values42.put(PRECIO_PRODUCTO, 74);
+        values42.put(ID_PRODUCTOS_TIENDA, 3);
+        values42.put(ID_PRODUCTOS_CATEGORIA, 2);
+
+        ContentValues values43 = new ContentValues();
+        values43.put(NOMBRE_PRODUCTO, "Zapatilla Forum Mid");
+        values43.put(DESCRIPCION_PRODUCTO, "Zapatilla runner");
+        values43.put(PRECIO_PRODUCTO, 110);
+        values43.put(ID_PRODUCTOS_TIENDA, 4);
+        values43.put(ID_PRODUCTOS_CATEGORIA, 4);
+
+        ContentValues values44 = new ContentValues();
+        values44.put(NOMBRE_PRODUCTO, "Chaqueta SST Fleece");
+        values44.put(DESCRIPCION_PRODUCTO, "Chaqueta de adidas");
+        values44.put(PRECIO_PRODUCTO, 75);
+        values44.put(ID_PRODUCTOS_TIENDA, 4);
+        values44.put(ID_PRODUCTOS_CATEGORIA, 2);
+
+        ContentValues values45 = new ContentValues();
+        values45.put(NOMBRE_PRODUCTO, "Gorro Pescador");
+        values45.put(DESCRIPCION_PRODUCTO, "Gorro de adidas verde");
+        values45.put(PRECIO_PRODUCTO, 33);
+        values45.put(ID_PRODUCTOS_TIENDA, 4);
+        values45.put(ID_PRODUCTOS_CATEGORIA, 2);
+
+        ContentValues values46 = new ContentValues();
+        values46.put(NOMBRE_PRODUCTO, "Zapatillas Air Jordan");
+        values46.put(DESCRIPCION_PRODUCTO, "De piel sintetica");
+        values46.put(PRECIO_PRODUCTO, 119.99);
+        values46.put(ID_PRODUCTOS_TIENDA, 5);
+        values46.put(ID_PRODUCTOS_CATEGORIA, 4);
+
+        ContentValues values47 = new ContentValues();
+        values47.put(NOMBRE_PRODUCTO, "Nike Dri-Fit");
+        values47.put(DESCRIPCION_PRODUCTO, "Sujetador deportivo");
+        values47.put(PRECIO_PRODUCTO, 47.99);
+        values47.put(ID_PRODUCTOS_TIENDA, 5);
+        values47.put(ID_PRODUCTOS_CATEGORIA, 4);
+
+        ContentValues values48 = new ContentValues();
+        values48.put(NOMBRE_PRODUCTO, "Nike Sportswear");
+        values48.put(DESCRIPCION_PRODUCTO, "Mochila pequeña de entrenamiento");
+        values48.put(PRECIO_PRODUCTO, 64.99);
+        values48.put(ID_PRODUCTOS_TIENDA, 5);
+        values48.put(ID_PRODUCTOS_CATEGORIA, 4);
+
+        ContentValues values49 = new ContentValues();
+        values49.put(NOMBRE_PRODUCTO, "Corte de pelo El pero");
+        values49.put(DESCRIPCION_PRODUCTO, "Degradado muy difuminado");
+        values49.put(PRECIO_PRODUCTO, 16);
+        values49.put(ID_PRODUCTOS_TIENDA, 6);
+        values49.put(ID_PRODUCTOS_CATEGORIA, 6);
+
+        ContentValues values50 = new ContentValues();
+        values50.put(NOMBRE_PRODUCTO, "Corte de pelo Mohicano");
+        values50.put(DESCRIPCION_PRODUCTO, "Como MA Baracus");
+        values50.put(PRECIO_PRODUCTO, 8);
+        values50.put(ID_PRODUCTOS_TIENDA, 6);
+        values50.put(ID_PRODUCTOS_CATEGORIA, 6);
+
+        ContentValues values51 = new ContentValues();
+        values51.put(NOMBRE_PRODUCTO, "Corte de pelo Pompadou");
+        values51.put(DESCRIPCION_PRODUCTO, "Tupe");
+        values51.put(PRECIO_PRODUCTO, 13);
+        values51.put(ID_PRODUCTOS_TIENDA, 6);
+        values51.put(ID_PRODUCTOS_CATEGORIA, 6);
 
         sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values31);
         sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values32);
         sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values33);
         sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values34);
         sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values35);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values36);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values37);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values38);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values39);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values40);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values41);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values42);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values43);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values44);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values45);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values46);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values47);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values48);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values49);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values50);
+        sqLiteDatabase.insert(TABLE_PRODUCTOS, null, values51);
 
     }
 
